7d5d8dd666439bb0cc12dbba80c96f0709ae6f83
amtech <amtechdigitalnetworks@gmail.com>
2023-12-01 15:25:14 +0100

    ADD, UPDATE: added the admin view and routes and also update some files

---
+++ resources/views/auth-users/login.blade.php
@@ -0,0 +1,54 @@
+@extends('auth-users.auth')
+
+@section('content')
+    <div class="container d-flex align-items-center justify-content-center form-height-login pt-24px pb-24px">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-10">
+                <div class="card">
+                    <div class="card-header bg-primary">
+                        <div class="ec-brand">
+                            <a href="index.php" title="Shop">
+                                <!-- <img class="ec-brand-icon" src="assets/img/logo/logo-login.png" alt="" /> -->
+                                <h1 class="text-white pt-3">Shop</h1>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body p-5">
+                        {{-- <h4 class="text-dark mb-5">logIn Your Account</h4> --}}
+
+                        <form action="{{ route('users.create') }}">
+                            <div class="row">
+                                <div class="form-group col-md-12 mb-4">
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="email">
+                                </div>
+
+                                <div class="form-group col-md-12 ">
+                                    <input name="password" type="password" class="form-control" id="password" placeholder="Password">
+                                </div>
+
+                                <div class="col-md-12">
+                                    <div class="d-flex my-2 justify-content-between">
+                                        <div class="d-inline-block mr-3">
+                                            <div class="control control-checkbox">Remember me
+                                                <input name="remember-me" type="checkbox" />
+                                                <div class="control-indicator"></div>
+                                            </div>
+                                        </div>
+
+                                        <p><a class="text-blue" href="{{ route('users.password-reset') }}">Forgot Password?</a></p>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary btn-block mb-4">LogIn</button>
+
+                                    <p class="sign-upp">Don't have an account yet ?
+                                        <a class="text-blue" href="{{ route('users.register') }}">Register</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
