a3ee10323236e6cd79e338b4072f5a5bc8632914
amtech <amtechdigitalnetworks@gmail.com>
2023-12-02 23:02:11 +0100

    UPDATE: update the migration from address to addresses

--- database/migrations/2023_12_01_105948_create_addresses_table.php
+++ database/migrations/2023_12_01_105948_create_addresses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('address', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('address');
+        Schema::dropIfExists('addresses');
     }
 };
