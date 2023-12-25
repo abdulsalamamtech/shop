4a90565018d0ed10426d6321ad7bf1e7864fee49
amtech <amtechdigitalnetworks@gmail.com>
2023-12-25 16:55:01 +0100

    UPDATE: update and modified some routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SubCategoryController;
 
@@ -68,10 +70,64 @@ Route::get('/contact', function () {
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
 
-    // Categories
-    Route::prefix('admin')->group(function(){
-        Route::prefix('categories')->group(function(){
 
+    Route::prefix('admin')->group(function(){
+
+        // Admin
+        Route::get('/', function(){
+            return view('admin.index');
+        })->name('admin');
+
+        // Customers and Users that aren't admin
+        Route::prefix('customers')->group(function(){
+            // Get all Customers
+            Route::get('/', [UserController::class, 'index'])->name('customers.index');
+            Route::get('/grid', [UserController::class, 'index'])->name('customers.index.grid');
+            // Route::get('/', [BrandController::class, 'index'])->name('brands.index');
+            // Show the form for creating a new resource.
+            // Route::get('/create', [BrandController::class, 'create'])->name('brands.create');
+            // Save new resource information to database
+            // Route::post('/', [BrandController::class, 'store'])->name('brands.store');
+            // Show the information of a resource
+            Route::get('/{customer}', [UserController::class, 'show'])->name('customers.show');
+            // Show the form to edit a resource
+            Route::get('/{customer}/edit', [UserController::class, 'edit'])->name('customers.edit');
+            // Update the resource
+            Route::put('/{customer}', [UserController::class, 'update'])->name('customers.update');
+            // Delete the resource
+            Route::delete('/{customer}', [UserController::class, 'destroy'])->name('customers.destroy');
+            // Activate the resource
+            Route::post('/{customer}/activate', [UserController::class, 'activate'])->name('customers.activate');
+            // Deactivate the resource
+            Route::post('/{customer}/deactivate', [UserController::class, 'deactivate'])->name('customers.deactivate');
+
+        });
+
+        // Brands
+        Route::prefix('brands')->group(function(){
+            // Get all Brands
+            Route::get('/', [BrandController::class, 'index'])->name('brands.index');
+            // Show the form for creating a new resource.
+            Route::get('/create', [BrandController::class, 'create'])->name('brands.create');
+            // Save new resource information to database
+            Route::post('/', [BrandController::class, 'store'])->name('brands.store');
+            // Show the information of a resource
+            Route::get('/{brand}', [BrandController::class, 'show'])->name('brands.show');
+            // Show the form to edit a resource
+            Route::get('/{brand}/edit', [BrandController::class, 'edit'])->name('brands.edit');
+            // Update the resource
+            Route::put('/{brand}', [BrandController::class, 'update'])->name('brands.update');
+            // Delete the resource
+            Route::delete('/{brand}', [BrandController::class, 'destroy'])->name('brands.destroy');
+            // Activate the resource
+            Route::post('/{brand}/activate', [BrandController::class, 'activate'])->name('brands.activate');
+            // Deactivate the resource
+            Route::post('/{brand}/deactivate', [BrandController::class, 'deactivate'])->name('brands.deactivate');
+
+        });
+
+        // Categories
+        Route::prefix('categories')->group(function(){
             // Get all categories
             Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
             // Show the form for creating a new resource.
@@ -86,8 +142,14 @@ Route::get('/contact', function () {
             Route::put('/{category}', [CategoryController::class, 'update'])->name('categories.update');
             // Delete the resource
             Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+            // Activate the resource
+            Route::post('/{category}/activate', [CategoryController::class, 'activate'])->name('categories.activate');
+            // Deactivate the resource
+            Route::post('/{category}/deactivate', [CategoryController::class, 'deactivate'])->name('categories.deactivate');
 
         });
+
+        // Sub Categories
         Route::prefix('sub-categories')->group(function(){
 
             // Get all categories
@@ -104,13 +166,40 @@ Route::get('/contact', function () {
             Route::put('/{sub_category}', [SubCategoryController::class, 'update'])->name('categories.sub.update');
             // Delete the resource
             Route::delete('/{sub_category}', [SubCategoryController::class, 'destroy'])->name('categories.sub.destroy');
+            // Activate the resource
+            Route::post('/{sub_category}/activate', [SubCategoryController::class, 'activate'])->name('categories.sub.activate');
+            // Deactivate the resource
+            Route::post('/{sub_category}/deactivate', [SubCategoryController::class, 'deactivate'])->name('categories.sub.deactivate');
+
+        });
+
+        // Products
+        Route::prefix('products')->group(function(){
+            // Get all categories
+            Route::get('/', [ProductController::class, 'index'])->name('products.index');
+            // Show the form for creating a new resource.
+            Route::get('/create', [ProductController::class, 'create'])->name('products.create');
+            // Save new resource information to database
+            Route::post('/', [ProductController::class, 'store'])->name('products.store');
+            // Show the information of a resource
+            Route::get('/{product}', [ProductController::class, 'show'])->name('products.show');
+            // Show the form to edit a resource
+            Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+            // Update the resource
+            Route::put('/{product}', [ProductController::class, 'update'])->name('products.update');
+            // Delete the resource
+            Route::delete('/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+            // Activate the resource
+            Route::post('/{product}/activate', [ProductController::class, 'activate'])->name('products.activate');
+            // Deactivate the resource
+            Route::post('/{product}/deactivate', [ProductController::class, 'deactivate'])->name('products.deactivate');
 
         });
     });
 
 
 
-    Route::get('/passsword-reset', function(){
+    Route::get('/password-reset', function(){
         return request()->all();
     })->name('auth.password-reset');
 
@@ -149,40 +238,40 @@ Route::get('/contact', function () {
 Route::prefix('admin')->group(function (){
 
     // Admin Dashboard
-    Route::get('/', function(){
-        return view('admin.index');
-    });
+    // Route::get('/', function(){
+    //     return view('admin.index');
+    // });
 
 
     // Admin Brands Route
-    Route::prefix('brands')->group(function (){
-        Route::get('/', function(){
-            return view('admin.brands.brand');
-        })->name('admin.brands.index');
-        Route::get('/add', function(){
-            return view('admin.brands.brand-add');
-        })->name('admin.brands.create');
-        Route::get('/info', function(){
-            return view('admin.brands.brand-info');
-        })->name('admin.brands.show');
-    });
+    // Route::prefix('brands')->group(function (){
+        // Route::get('/', function(){
+        //     return view('admin.brands.brand');
+        // })->name('admin.brands.index');
+        // Route::get('/add', function(){
+        //     return view('admin.brands.brand-add');
+        // })->name('admin.brands.create');
+        // Route::get('/info', function(){
+        //     return view('admin.brands.brand-info');
+        // })->name('admin.brands.show');
+    // });
 
 
     // Admin Customers Route
-    Route::prefix('customers')->group(function (){
-        Route::get('/', function(){
-            return view('admin.customers.customer');
-        })->name('customers.index');
-        Route::get('/grid', function(){
-            return view('admin.customers.customer-grid');
-        });
-        Route::get('/info', function(){
-            return view('admin.customers.customer-info');
-        });
-        Route::get('/detail', function(){
-            return view('admin.customers.customer-info');
-        });
-    });
+    // Route::prefix('customers')->group(function (){
+    //     Route::get('/', function(){
+    //         return view('admin.customers.customer');
+    //     })->name('customers.index');
+    //     Route::get('/grid', function(){
+    //         return view('admin.customers.customer-grid');
+    //     });
+    //     Route::get('/info', function(){
+    //         return view('admin.customers.customer-info');
+    //     });
+    //     Route::get('/detail', function(){
+    //         return view('admin.customers.customer-info');
+    //     });
+    // });
 
 
     // Admin Orders Route
@@ -217,28 +306,28 @@ Route::prefix('admin')->group(function (){
     });
 
     // Admin Categories Route
-    Route::prefix('products')->group(function (){
+    // Route::prefix('products')->group(function (){
 
-        Route::get('/', function(){
-            return view('admin.products.product');
-        });
-        Route::get('/add', function(){
-            return view('admin.products.product-add');
-        });
-        Route::get('/info', function(){
-            return view('admin.products.product-detail');
-        });
-        Route::get('/detail', function(){
-            return view('admin.products.product-detail');
-        });
-        Route::get('/edit', function(){
-            return view('admin.products.product-edit');
-        });
-        Route::get('/grid', function(){
-            return view('admin.products.product-grid');
-        });
+    //     Route::get('/', function(){
+    //         return view('admin.products.product');
+    //     });
+    //     Route::get('/add', function(){
+    //         return view('admin.products.product-add');
+    //     });
+    //     Route::get('/info', function(){
+    //         return view('admin.products.product-detail');
+    //     });
+    //     Route::get('/detail', function(){
+    //         return view('admin.products.product-detail');
+    //     });
+    //     Route::get('/edit', function(){
+    //         return view('admin.products.product-edit');
+    //     });
+    //     Route::get('/grid', function(){
+    //         return view('admin.products.product-grid');
+    //     });
 
-    });
+    // });
 
     // Admin Mail Route
     Route::prefix('mails')->group(function (){
